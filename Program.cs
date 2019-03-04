492001d4135d5e7ccde467563eac8869326caa11
Warin12 <evanw8@learn.cssd.ab.ca>
2019-03-04 14:27:05 -0700

    --all

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace Luckysevens
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+         int totalnumberofsevens = 0;
+         Random rng = new Random();
+
+         int die1;
+         int die2;
+          
+         for (int i = 1; i <= 100; i++)
+            {
+                die1 = rng.Next(1, 7);
+                die2 = rng.Next(1,7);
+
+                if(die1 + die2 == 7)
+                 totalnumberofsevens++;
+            }
+        Console.WriteLine("Out of 100 rolls, you rolled {0} Sevens", totalnumberofsevens);
+        Console.ReadLine();
         }
-    }
+    } 
 }
